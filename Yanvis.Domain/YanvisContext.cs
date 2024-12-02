aaa19e61d7fda3e64b600e30bf8329d08a4c884c
Lysko Denis <denislysko@yandex.ru>
2024-12-02 22:51:40 +0300

    feat: Удаление старого кода

--- Yanvis.Domain/YanvisContext.cs
+++ Yanvis.Domain/YanvisContext.cs
@@ -19,22 +19,3 @@ public class YanvisContext : DbContext
 
     }
 }
-
-
-//public class UserContext : DbContext
-//{
-//    public DbSet<User> Users { get; set; }
-//    public string Schemes { get; set; }
-//    public UserContext(DbContextOptions<YanvisContext> options) : base(options)
-//    {
-//    }
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//    {
-//        optionsBuilder.UseNpgsql("Host=localhost;Database=mydb;Username=myuser;Password=mypassword");
-//    }
-
-//    protected override void OnModelCreating(ModelBuilder modelBuilder)
-//    {
-
-//    }
-//}
